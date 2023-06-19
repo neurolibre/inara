1d3b05965a7db42f69a749f6ca576d07f87250f4
Agah <agahkarakuzu@gmail.com>
2023-06-19 16:51:55 -0400

    Change joss -> neurolibre

--- data/custom/cff.lua
+++ data/custom/cff.lua
@@ -45,7 +45,7 @@ local function preferred_citation(meta)
     -- ['start'] = article.fpage,
     ['title'] = stringify(meta.title),
     ['type'] = 'preprint',
-    ['url'] = ('https://joss.theoj.org/papers/%s'):format(stringify(article.doi)),
+    ['url'] = ('https://neurolibre.org/papers/%s'):format(stringify(article.doi)),
     -- ['volume'] = article.volume,
   }
 end
