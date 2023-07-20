8da392c2a0d874aadecd09f9e455dbf70a9cd5b2
Agah <agahkarakuzu@gmail.com>
2023-07-20 15:12:59 -0400

    Update the lua filter to pass num auths to latexx

--- data/filters/draft.lua
+++ data/filters/draft.lua
@@ -6,6 +6,14 @@ function Meta (meta)
     meta.article.volume = '0'
     meta.published = 'unpublished'
     meta.published_parts = os.date('*t')
+    meta.author_number = CountAuthors(meta.authors)
     return meta
+  end
+end
+
+function CountAuthors(elem)
+  if elem.t == 'MetaList' and elem[1].t == 'MetaInlines' then
+    local count = #elem
+    return pandoc.MetaList({pandoc.MetaInlines(pandoc.SmallCaps({pandoc.Str(tostring(count))}))})
   end
 end
