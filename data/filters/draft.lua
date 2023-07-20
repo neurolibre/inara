04b375f93ccfe28d0110485e3d1210c7e2d403cd
Agah <agahkarakuzu@gmail.com>
2023-07-20 19:28:31 -0400

    I have no idea about Lua

--- data/filters/draft.lua
+++ data/filters/draft.lua
@@ -1,3 +1,10 @@
+local function CountAuthors(elem)
+  if elem.t == 'MetaList' and elem[1].t == 'MetaInlines' then
+    local count = #elem
+    return pandoc.MetaList({pandoc.MetaInlines(pandoc.SmallCaps({pandoc.Str(tostring(count))}))})
+  end
+end
+
 --- Removes and alters metadata for draft output
 function Meta (meta)
   if meta.draft and meta.draft ~= '' then
@@ -8,12 +15,5 @@ function Meta (meta)
     meta.published_parts = os.date('*t')
     meta.author_number = CountAuthors(meta.authors)
     return meta
-  end
-end
-
-local function CountAuthors(elem)
-  if elem.t == 'MetaList' and elem[1].t == 'MetaInlines' then
-    local count = #elem
-    return pandoc.MetaList({pandoc.MetaInlines(pandoc.SmallCaps({pandoc.Str(tostring(count))}))})
   end
 end
