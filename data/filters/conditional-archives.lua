b9fee9fb9b7ee2680a1a600d4e7dbb0c42120982
Agah <agahkarakuzu@gmail.com>
2024-12-11 00:45:51 -0500

    Debug messages

--- data/filters/conditional-archives.lua
+++ data/filters/conditional-archives.lua
@@ -1,10 +1,16 @@
 function Meta(meta)
     -- Check if each DOI is present and not set to 'N/A'
+    print("Repository DOI:", meta.repository_doi)
+    print("Data DOI:", meta.data_doi)
+    print("Book DOI:", meta.book_doi)
+    print("Docker DOI:", meta.docker_doi)
+    print("Software Review URL:", meta.software_review_url)
     if meta.repository_doi and meta.repository_doi ~= 'N/A' then
       meta.include_repository_doi = true
     end
     if meta.data_doi and meta.data_doi ~= 'N/A' then
       meta.include_data_doi = true
+      print("Data DOI is present and not N/A")
     end
     if meta.book_doi and meta.book_doi ~= 'N/A' then
       meta.include_book_doi = true
