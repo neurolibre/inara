b06775bea711bb33f772d4703298a1be910db32f
Agah <agahkarakuzu@gmail.com>
2024-12-11 00:56:39 -0500

    more debug

--- data/filters/conditional-archives.lua
+++ data/filters/conditional-archives.lua
@@ -1,28 +1,40 @@
 function Meta(meta)
     -- Check if each DOI is present and not set to 'N/A'
-    print("Repository DOI:", meta.repository_doi)
-    print("Data DOI:", meta.data_doi)
-    print("Book DOI:", meta.book_doi)
-    print("Docker DOI:", meta.docker_doi)
-    print("Software Review URL:", meta.software_review_url)
-    if meta.repository_doi and meta.repository_doi ~= 'N/A' then
+    print("Repository DOI value:", tostring(meta.repository_doi and meta.repository_doi.text or "nil"))
+    if meta.repository_doi and meta.repository_doi.text ~= 'N/A' then
       meta.include_repository_doi = true
+      print("Including repository DOI")
     end
-    if meta.data_doi and meta.data_doi ~= 'N/A' then
+
+    print("Data DOI value:", tostring(meta.data_doi and meta.data_doi.text or "nil"))
+    if meta.data_doi and meta.data_doi.text ~= 'N/A' then
       meta.include_data_doi = true
-      print("Data DOI is present and not N/A")
+      print("Including data DOI")
     end
-    if meta.book_doi and meta.book_doi ~= 'N/A' then
+
+    print("Book DOI value:", tostring(meta.book_doi and meta.book_doi.text or "nil"))
+    if meta.book_doi and meta.book_doi.text ~= 'N/A' then
       meta.include_book_doi = true
+      print("Including book DOI")
     end
-    if meta.docker_doi and meta.docker_doi ~= 'N/A' then
+
+    print("Docker DOI value:", tostring(meta.docker_doi and meta.docker_doi.text or "nil"))
+    if meta.docker_doi and meta.docker_doi.text ~= 'N/A' then
       meta.include_docker_doi = true
+      print("Including docker DOI")
     end
-    if meta.software_review_url and meta.software_review_url ~= 'N/A' then
+
+    print("Software review URL value:", tostring(meta.software_review_url and meta.software_review_url.text or "nil"))
+    if meta.software_review_url and meta.software_review_url.text ~= 'N/A' then
       meta.include_software_review = true
+      print("Including software review")
     end
-    if meta.book_exec_url and meta.book_exec_url ~= 'N/A' then
+
+    print("Book exec URL value:", tostring(meta.book_exec_url and meta.book_exec_url.text or "nil"))
+    if meta.book_exec_url and meta.book_exec_url.text ~= 'N/A' then
       meta.include_book_exec = true
+      print("Including book exec")
     end
+
     return meta
 end
