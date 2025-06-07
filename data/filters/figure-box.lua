1fa83caa28f49676c7f9d91bac382a7647d751c8
agahkarakuzu <agahkarakuzu@gmail.com>
2025-06-07 10:59:34 -0400

    one more chance

--- data/filters/figure-box.lua
+++ data/filters/figure-box.lua
@@ -1,45 +1,53 @@
-function RawBlock(el)
-  if el.format ~= "markdown" then return end
-
-  local lines = {}
-  for line in el.text:gmatch("[^\r\n]*") do
-    table.insert(lines, line)
-  end
-
-  local output_blocks = {}
-  local i = 1
-  while i <= #lines do
-    -- Match opening: :::{figure} or :::{figure} #id
-    local open, id = lines[i]:match("^:::%s*{?figure}?%s*#?([%w%-_]*)")
-    if lines[i]:match("^:::%s*{?figure}?") then
-      -- Found opening
-      local figure_id = lines[i]:match("#([%w%-_]+)")
-      local content_lines = {}
-      i = i + 1
-      -- Collect until closing ::: or end of file
-      while i <= #lines and not lines[i]:match("^:::%s*$") do
-        table.insert(content_lines, lines[i])
+-- Process the entire document to find and replace :::{figure} patterns
+function Pandoc(doc)
+    local newblocks = {}
+    local i = 1
+    
+    while i <= #doc.blocks do
+        local block = doc.blocks[i]
+        local blocktext = pandoc.utils.stringify(block)
+        
+        -- Check if this block contains :::{figure}
+        if blocktext:match(":::{figure}") then
+            -- This block starts a figure, collect all blocks until we find closing :::
+            local figure_blocks = {}
+            local figure_id = blocktext:match(":::{figure}%s*#([%w%-_]+)")
+            
+            -- Skip the opening :::{figure} line
+            i = i + 1
+            
+            -- Collect content blocks until we find closing :::
+            while i <= #doc.blocks do
+                local content_block = doc.blocks[i]
+                local content_text = pandoc.utils.stringify(content_block)
+                
+                if content_text:match("^:::%s*$") then
+                    -- Found closing :::, stop collecting
+                    break
+                else
+                    -- Add this block's content to figure content
+                    table.insert(figure_blocks, content_text)
+                end
+                i = i + 1
+            end
+            
+            -- Create LaTeX box with collected content
+            local content = table.concat(figure_blocks, "\n")
+            local latex = "\\begin{tcolorbox}[colback=gray!5!white,colframe=gray!75!black"
+            if figure_id then
+                latex = latex .. ",title=Figure \\label{" .. figure_id .. "}"
+            end
+            latex = latex .. "]\n" .. content .. "\n\\end{tcolorbox}"
+            
+            table.insert(newblocks, pandoc.RawBlock("latex", latex))
+        else
+            -- Not a figure block, keep as is
+            table.insert(newblocks, block)
+        end
+        
         i = i + 1
-      end
-      -- Skip the closing ::: if present
-      if i <= #lines and lines[i]:match("^:::%s*$") then
-        i = i + 1
-      end
-      -- Prepare LaTeX box
-      local latex = "\\begin{tcolorbox}[colback=gray!5!white,colframe=gray!75!black"
-      if figure_id then
-        latex = latex .. ",title=Figure \\label{" .. figure_id .. "}"
-      end
-      latex = latex .. "]\n" .. table.concat(content_lines, "\n") .. "\n\\end{tcolorbox}"
-      table.insert(output_blocks, pandoc.RawBlock("latex", latex))
-    else
-      -- Not a figure block, output as markdown
-      if lines[i] ~= "" then
-        table.insert(output_blocks, pandoc.RawBlock("markdown", lines[i]))
-      end
-      i = i + 1
     end
-  end
-
-  return output_blocks
+    
+    doc.blocks = newblocks
+    return doc
 end
