fcd0c9f1bcde6d215782826830c7e202153fcde6
agahkarakuzu <agahkarakuzu@gmail.com>
2025-06-07 10:48:39 -0400

    Raw txt based filtering

--- data/filters/figure-box.lua
+++ data/filters/figure-box.lua
@@ -1,20 +1,45 @@
-function Div(el)
-    -- Check for divs with the class "figure"
-    if el.classes:includes("figure") then
-        -- Get the content of the div
-        local content = pandoc.utils.stringify(el.content)
-        
-        -- Create a LaTeX box with the content
-        local box_content = "\\begin{tcolorbox}[colback=gray!5!white,colframe=gray!75!black,title=Figure"
-        
-        -- Add the ID as a label if it exists
-        if el.identifier and el.identifier ~= "" then
-            box_content = box_content .. " \\label{" .. el.identifier .. "}"
-        end
-        
-        box_content = box_content .. "]\n" .. content .. "\n\\end{tcolorbox}"
-        
-        -- Return the content wrapped in a LaTeX box
-        return pandoc.RawBlock("latex", box_content)
+function RawBlock(el)
+  if el.format ~= "markdown" then return end
+
+  local lines = {}
+  for line in el.text:gmatch("[^\r\n]*") do
+    table.insert(lines, line)
+  end
+
+  local output_blocks = {}
+  local i = 1
+  while i <= #lines do
+    -- Match opening: :::{figure} or :::{figure} #id
+    local open, id = lines[i]:match("^:::%s*{?figure}?%s*#?([%w%-_]*)")
+    if lines[i]:match("^:::%s*{?figure}?") then
+      -- Found opening
+      local figure_id = lines[i]:match("#([%w%-_]+)")
+      local content_lines = {}
+      i = i + 1
+      -- Collect until closing ::: or end of file
+      while i <= #lines and not lines[i]:match("^:::%s*$") do
+        table.insert(content_lines, lines[i])
+        i = i + 1
+      end
+      -- Skip the closing ::: if present
+      if i <= #lines and lines[i]:match("^:::%s*$") then
+        i = i + 1
+      end
+      -- Prepare LaTeX box
+      local latex = "\\begin{tcolorbox}[colback=gray!5!white,colframe=gray!75!black"
+      if figure_id then
+        latex = latex .. ",title=Figure \\label{" .. figure_id .. "}"
+      end
+      latex = latex .. "]\n" .. table.concat(content_lines, "\n") .. "\n\\end{tcolorbox}"
+      table.insert(output_blocks, pandoc.RawBlock("latex", latex))
+    else
+      -- Not a figure block, output as markdown
+      if lines[i] ~= "" then
+        table.insert(output_blocks, pandoc.RawBlock("markdown", lines[i]))
+      end
+      i = i + 1
     end
+  end
+
+  return output_blocks
 end
